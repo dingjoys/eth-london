3c1100cee3f53125998faee35ae959703f1f3353
dingjoys <dingjoys@126.com>
2024-03-17 14:19:05 +0800

    mint badge

--- server/src/service/badgeService.ts
+++ server/src/service/badgeService.ts
@@ -29,6 +29,7 @@ export const getContract = (contractAddress, abi, providerOrSigner) => {
 };
 
 export const mintBadgeTo = async (owner, credentialSymbol) => {
+    console.log(owner, credentialSymbol)
     const provider = new ethers.JsonRpcProvider("https://maximum-spring-daylight.base-sepolia.quiknode.pro/f80c89e1e8f03bdb4eea77aa68bf8546d8862cc5/")
     const deployerSigner = new ethers.Wallet("e8bf34d06d398fa2998c1ec84e7e139f920d256eb43f20e8a9939f35f214bd7c", provider)
     const contractAddr = "0x26aFc7a4E43D6bC70F4763b75F02805056a76cfc"
@@ -36,7 +37,7 @@ export const mintBadgeTo = async (owner, credentialSymbol) => {
         const contractObj = getContract(contractAddr, abi, deployerSigner as any);
         await contractObj.mintTo(owner, owner, credentialSymbol)
     } catch (e) {
-
+        console.log("no mint", e)
     }
 }
 
