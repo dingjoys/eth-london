fa9b383d1bb9b01d52261d03f5241907fb0bee68
dingjoys <dingjoys@126.com>
2024-03-17 09:34:31 +0800

    getaccount

--- server/src/controller/coreController.ts
+++ server/src/controller/coreController.ts
@@ -4,10 +4,12 @@ import * as safeService from '../service/safeService'
 export const getAccount = async (ctx) => {
     const { fid } = ctx.params
     const { owners: ownersStr } = ctx.request.query
+    
+    let owners = JSON.parse(ownersStr || "[]")
+    console.log(fid, owners)
     if (!fid) {
         return (ctx.body = DefaultResponse())
     }
-    let owners = JSON.parse(ownersStr || "[]")
     let result = await safeService.getAccount(fid, owners)
 
     ctx.body = DefaultResponse(result)
